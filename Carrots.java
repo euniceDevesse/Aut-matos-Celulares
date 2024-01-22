f44b7443b1c8128737c6050efacff0210b329cab
Leonel Figueira Caetano <120415839+Marimba369@users.noreply.github.com>
2024-01-22 01:59:07 +0000

    Update Carrots.java

--- Carrots.java
+++ Carrots.java
@@ -1,4 +1,22 @@
 public class Carrots extends Piece
 {
-    
+    private Boolean eaten_flag;
+
+    public Carrots(int type)
+    {
+        super(type);
+        this.eaten_flag = false;
+    }
+
+    public Boolean getEaten()
+    {
+        return this.eaten_flag;
+    }
+
+    public void setEaten()
+    {
+        this.eaten_flag = true;
+    }
+
+    public 
 }
