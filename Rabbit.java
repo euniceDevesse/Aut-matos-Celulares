918c5ef9ba8839476a8e10fd35af4968a8aacf35
Leonel Figueira Caetano <120415839+Marimba369@users.noreply.github.com>
2024-01-22 01:58:24 +0000

    Update Rabbit.java

--- Rabbit.java
+++ Rabbit.java
@@ -1,5 +1,19 @@
 
+
 public class Rabbit extends Piece
 {
-    
+    public Rabbit(int type, int timeStep)
+    {
+        super(type, timeStep);
+    }
+
+    public void toEat()
+    {
+        setTimeStep(1);
+    }
+
+    public void incremetTime()
+    {
+        setTimeStep(getTimeStep() + 1);
+    }
 }
